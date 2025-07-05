19c8d80997c824336aaaf24e270daa01350bafe0
Aa-ra-dhana5 <aaradhanaparmar55@gmail.com>
2025-07-05 10:57:00 +0530

    Day3 update

---
+++ Day3/Login/LoginPage/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using LoginPage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginPage.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Username == "admin" && model.Password == "admin123")
+                {
+                    TempData["Message"] = "Login successful!";
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    ViewBag.Error = "Invalid username or password";
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
